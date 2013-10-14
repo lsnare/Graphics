0de5893c608a080aead344366e51694915b71aa2
Lucian Snare <lsnare@lhup.edu>
2013-10-14 11:14:33 -0400

    Koch!

--- TurtleGraphic/src/Dragon.java
+++ TurtleGraphic/src/Dragon.java
@@ -38,14 +38,45 @@ public class Dragon {
 		}
 	}
 	
+	public void koch(int n){
+		if (n==0){
+			turtle.forward(distance);
+		} else {
+			koch(n-1);
+			turtle.left(60);
+			koch(n-1);
+			turtle.left(-120);
+			koch(n-1);
+			turtle.left(60);
+			koch(n-1);
+		}
+		
+	}
+	
 	public static void main(String[] args){
 		Turtle turtle = new Turtle();
 		Dragon dragon = new Dragon(turtle, 5);
-		turtle.init(350, 230, 0);
+		turtle.init(500, 400, 0);
 		turtle.pen(true);
 		
-		dragon.dragon(16);
-	
+		//dragon.dragon(16);
+		dragon.koch(4);
+		turtle.left(-120);
+		dragon.koch(4);
+		turtle.left(-120);
+		dragon.koch(4);
+		
+		dragon.koch(4);
+		turtle.left(-120);
+		dragon.koch(4);
+		turtle.left(-120);
+		dragon.koch(4);
+		
+		dragon.koch(4);
+		turtle.left(-120);
+		dragon.koch(4);
+		turtle.left(-120);
+		dragon.koch(4);
 		turtle.pen(false);
 		turtle.show();
 		
