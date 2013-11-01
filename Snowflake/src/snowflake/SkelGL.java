756858770d56fe58f8ecddce054efc3e9f71aeb7
Lucian Snare <lsnare@lhup.edu>
2013-11-01 11:25:30 -0400

    Whoa

--- Snowflake/src/snowflake/SkelGL.java
+++ Snowflake/src/snowflake/SkelGL.java
@@ -1,3 +1,4 @@
+
 package snowflake;
 
 import org.eclipse.swt.SWT;
@@ -13,231 +14,241 @@ import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
-import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GLContext;
-import org.lwjgl.util.glu.GLU;
 
 public abstract class SkelGL {
-	/* animation indicator for menu selector */
-	boolean animate = true;
-
-	
-	/* time at last frame */
-	long lastFrame;
-	
-	/* frames per second */
-	int fps;
-	
-	/* last fps time */
-	long lastFPS;
-	
-	
-	// SVGA 800x600
-	// XVGA 1024x768
-	int WIDTH = 1024, HEIGHT = 768;
-	
-	/* Eye location */
-	float eyex = 0f, eyey = 0f, eyez = 2f;
-	float zNear = 0.01f, zFar = 1f;
-	/*
-	 * Window manager parameters
-	 */
-	Display display = new Display();
-	Shell shell;
-	Composite comp;
-	GLData data = new GLData();
-	GLCanvas canvas;
-	/**
-	 * Get the accurate time system
-	 * 
-	 */
-	public long getTime(){		
-		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
-	}
-	
-	/**
-	 * Calculate how many milliseconds has elapsed since last frame
-	 */
-	
-	public int getDelta(){
-		long time = getTime();
-		int delta = (int) (time - lastFrame);
-		lastFrame = time;
-		
-		return delta;
-	}
-	
-	public void updateFPS(){
-		if (getTime() - lastFPS > 1000){
-			shell.setText("The rotating Cube - FPS: " + fps);
-			fps = 0;
-			lastFPS += 1000;
-		}
-		fps++;
-	}
-	
-	public void start() {
-		
-		// Initialize the Window Manager
-		initSWT();
-		
-		initGL();
-		
-		// Call all timing initializations
-		getDelta();
-		lastFPS = getTime();
-		
-		// Start a thread that loops and
-		// handles frame generation
-		final Runnable frameHandler = new Runnable(){
-
-			@Override
-			public void run() {
-				// Software to handle frame creation
-				// runs per frame
-				if (!canvas.isDisposed()){
-					canvas.setCurrent();
-					
-					try {
-						GLContext.useContext(canvas);
-					} catch (LWJGLException e) {
-						System.err.println("Context error!");
-						System.exit(-1);
-					}
-					int delta = getDelta();
-					
-					update(delta);
-					renderGL();
-					canvas.swapBuffers();
-					
-					//display.timerExec(50, this);
-					//Run this asynchronously
-					display.asyncExec(this);
-				}
-			}
-		}; // End runnable
-		canvas.addListener(SWT.Paint, new Listener(){
-
-			@Override
-			public void handleEvent(Event event) {
-				frameHandler.run();
-			}
-		});
-		
-		canvas.addListener(SWT.Resize, new Listener(){
-			public void handleEvent(Event event) {
-				try {
-					GLContext.useContext(canvas);
-				} catch (LWJGLException e){
-					e.printStackTrace();
-				}
-				// per drawing reset
-				resetGL();
-			}
-		});
-		
-		display.asyncExec(frameHandler);
-		
-		while(!shell.isDisposed()){
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-		display.dispose();
-	}
-	
-	private void initSWT() {
-
-		shell = new Shell(display);
-		shell.setLayout(new FillLayout());
-		
-		// Create a composite
-		comp = new Composite(shell, SWT.BORDER);
-		comp.setLayout(new FillLayout());
-		
-		// Depth size 
-		data.depthSize = 1;
-		data.doubleBuffer = true;
-		
-		// set up canvas
-		canvas = new GLCanvas(comp, SWT.NONE, data);
-		canvas.setCurrent();
-		canvas.setMenu(initMenu());
-		
-		shell.addListener(SWT.KeyDown, new Listener(){
-			public void handleEvent(Event e){
-				onKbdEvent(e);
-			}
-		});
-		
-		shell.addListener(SWT.MouseWheel, new Listener(){
-
-			@Override
-			public void handleEvent(Event event) {
-				if (event.count > 0)
-					eyez -= 0.05f;
-				else
-					eyez += 0.05f;
-			}
-			
-		});
-		shell.setText("WT/LWJGL Basic Cube");
-		shell.setSize(WIDTH, HEIGHT);
-		shell.open();
-		
-	}
-	
-	private void onKbdEvent(Event e) {
-
-		int key = e.keyCode;
-		
-		switch(key) {
-		case SWT.ARROW_UP:
-			eyez -= 0.01f;
-			break;
-			
-		case SWT.ARROW_DOWN:
-			eyez += 0.01f;
-			break;
-			
-		case SWT.ESC:
-			shell.dispose();
-			break;
-		}
-	}
-	
-	public Menu initMenu(){
-		
-		// Creates a pop-up menu
-		Menu menu = new Menu(shell, SWT.POP_UP);
-		// Add items to it
-		final MenuItem item0 = new MenuItem( menu, SWT.PUSH);
-
-		item0.setText(animate ? "Stop": "Animate");
-		
-		item0.addListener(SWT.Selection, new Listener(){
-			@Override
-			public void handleEvent(Event event) {
-				animate = !animate;
-				item0.setText(animate ? "Stop": "Animate");
-			}
-			
-		});
-		
-		MenuItem item1 = new MenuItem(menu, SWT.PUSH);
-		item1.setText("Exit");
-		item1.addListener(SWT.Selection, new Listener(){
-			@Override
-			public void handleEvent(Event event) {
-				shell.dispose();
-			}
-		});	
-		return menu;
-	}
-	
-	protected abstract void initGL();
-	protected abstract void update(int delta);
-	protected abstract void resetGL();
-	protected abstract void renderGL();
-	protected abstract void initTextures();
-	
+/* animation indicator for menu selector */
+boolean animate = true;
+ 
+/* time at last frame */
+long lastFrame;
+ 
+/* frames per second */
+int fps;
+ 
+/* last fps time */
+long lastFPS;
+ 
+ 
+// SVGA 800x600
+// XVGA 1024x768
+int WIDTH = 1024, HEIGHT = 768;
+ 
+/* Eye location */
+float eyex = 0, eyey = 0, eyez = 1;
+float zNear = 0.01f, zFar = 1000f;
+float fovy = 60f;
+/*
+* Window manager parameters
+*/
+Display display = new Display();
+Shell shell;
+Composite comp;
+GLData data = new GLData();
+GLCanvas canvas;
+/**
+* Get the accurate time system
+* 
+*/
+public long getTime(){
+return (Sys.getTime() * 1000) / Sys.getTimerResolution();
 }
+ 
+/**
+* Calculate how many milliseconds has elapsed since last frame
+*/
+ 
+public int getDelta(){
+long time = getTime();
+int delta = (int) (time - lastFrame);
+lastFrame = time;
+ 
+return delta;
+}
+ 
+public void updateFPS(){
+if (getTime() - lastFPS > 1000){
+shell.setText("The rotating Cube - FPS: " + fps);
+fps = 0;
+lastFPS += 1000;
+}
+fps++;
+}
+ 
+public void start() {
+ 
+// Initialize the Window Manager
+initSWT();
+ 
+initGL();
+ 
+// Call all timing initializations
+getDelta();
+lastFPS = getTime();
+ 
+// Start a thread that loops and
+// handles frame generation
+final Runnable frameHandler = new Runnable(){
+
+@Override
+public void run() {
+// Software to handle frame creation
+// runs per frame
+if (!canvas.isDisposed()){
+canvas.setCurrent();
+ 
+try {
+GLContext.useContext(canvas);
+} catch (LWJGLException e) {
+System.err.println("Context error!");
+System.exit(-1);
+}
+int delta = getDelta();
+ 
+update(delta);
+updateFPS();
+ 
+renderGL();
+canvas.swapBuffers();
+ 
+//display.timerExec(50, this);
+//Run this asynchronously
+display.asyncExec(this);
+}
+}
+}; // End runnable
+canvas.addListener(SWT.Paint, new Listener(){
+
+@Override
+public void handleEvent(Event event) {
+resetGL();
+}
+});
+ 
+canvas.addListener(SWT.Resize, new Listener(){
+public void handleEvent(Event event) {
+// per drawing reset
+resetGL();
+}
+});
+ 
+display.asyncExec(frameHandler);
+ 
+while(!shell.isDisposed()){
+if (!display.readAndDispatch())
+display.sleep();
+}
+display.dispose();
+}
+ 
+private void initSWT() {
+
+shell = new Shell(display);
+shell.setLayout(new FillLayout());
+ 
+// Create a composite
+comp = new Composite(shell, SWT.BORDER);
+comp.setLayout(new FillLayout());
+ 
+// Depth size 
+data.depthSize = 1;
+data.doubleBuffer = true;
+ 
+// set up canvas
+canvas = new GLCanvas(comp, SWT.NONE, data);
+canvas.setCurrent();
+canvas.setMenu(initMenu());
+ 
+try {
+GLContext.useContext(canvas);
+} catch (LWJGLException e){
+System.err.println("Graphics Context setting error!");
+System.exit(-1);
+}
+shell.addListener(SWT.KeyDown, new Listener(){
+public void handleEvent(Event e){
+onKbdEvent(e);
+}
+});
+ 
+shell.addListener(SWT.MouseWheel, new Listener(){
+
+@Override
+public void handleEvent(Event event) {
+if (event.count > 0)
+eyez -= 0.05f;
+else
+eyez += 0.05f;
+}
+ 
+});
+shell.setText("WT/LWJGL Snowflake");
+shell.setSize(WIDTH, HEIGHT);
+shell.open();
+ 
+}
+ 
+private void onKbdEvent(Event e) {
+
+int key = e.keyCode;
+ 
+switch(key) {
+case SWT.ARROW_UP:
+eyez -= 0.05f;
+break;
+ 
+case SWT.ARROW_DOWN:
+eyez += 0.05f;
+break;
+ 
+case SWT.ARROW_LEFT:
+eyex -= 0.05f;
+break;
+ 
+case SWT.ARROW_RIGHT:
+eyex += 0.05f;
+break;
+ 
+case SWT.ESC:
+shell.dispose();
+break;
+}
+}
+ 
+public Menu initMenu(){
+ 
+// Creates a pop-up menu
+Menu menu = new Menu(shell, SWT.POP_UP);
+// Add items to it
+final MenuItem item0 = new MenuItem( menu, SWT.PUSH);
+
+item0.setText(animate ? "Stop": "Animate");
+ 
+item0.addListener(SWT.Selection, new Listener(){
+@Override
+public void handleEvent(Event event) {
+animate = !animate;
+item0.setText(animate ? "Stop": "Animate");
+}
+ 
+});
+ 
+MenuItem item1 = new MenuItem(menu, SWT.PUSH);
+item1.setText("Exit");
+item1.addListener(SWT.Selection, new Listener(){
+@Override
+public void handleEvent(Event event) {
+shell.dispose();
+}
+});
+return menu;
+}
+ 
+protected abstract void initGL();
+protected abstract void update(int delta);
+protected abstract void resetGL();
+protected abstract void renderGL();
+protected abstract void initTextures();
+ 
+}
+
--- Snowflake/src/snowflake/Snowflake.java
+++ Snowflake/src/snowflake/Snowflake.java
@@ -1,3 +1,4 @@
+
 package snowflake;
 
 import org.eclipse.swt.graphics.Rectangle;
@@ -7,169 +8,158 @@ import org.lwjgl.opengl.GLContext;
 import org.lwjgl.util.glu.GLU;
 import org.lwjgl.util.vector.Vector2f;
 
-public class Snowflake extends SkelGL{
+public class Snowflake extends SkelGL {
+ 
+private int numTimesToSubdivide = 5;
+private int numFlakes = (int)Math.pow(4, numTimesToSubdivide);
+private int numVertices = 3 * numFlakes;
+ 
+private class KochSet{
+Vector2f l = null;
+Vector2f m = null;
+Vector2f n = null;
+Vector2f o = null;
+Vector2f p = null;
+}
+ 
+double deg60 = 60.0f * Math.PI / 180.0;
+ 
+private int index = 0;
+ 
+Vector2f[] point = new Vector2f[numVertices];
+ 
+public KochSet computeKochSet(Vector2f s, Vector2f d){
+KochSet k = new KochSet();
+k.l = s;
+k.m = new Vector2f(s.x + (d.x - s.x)/3.0f, s.y + (d.y - s.y)/3.0f);
+k.o = new Vector2f(s.x + (d.x - s.x)*2/3.0f, s.y + (d.y - s.y)*2/3.0f);
+float px = (float)(Math.cos(deg60) * (k.o.x - k.m.x) -  Math.sin(deg60) * (k.o.y - k.m.y)+ k.m.x);
+float py = (float)(Math.sin(deg60) * (k.o.x - k.m.x) +  Math.cos(deg60) * (k.o.y - k.m.y)+ k.m.y);
+k.n = new Vector2f(px, py);
+k.p = d;
+return k;
+}
+ 
+public void divideSide(Vector2f a, Vector2f b, int count){
+ 
+if (count > 0){
+KochSet kab = computeKochSet(a, b);
+ 
+divideSide(kab.l, kab.m, count -1);
+divideSide(kab.m, kab.n, count -1);
+divideSide(kab.n, kab.o, count -1);
+divideSide(kab.o, kab.p, count -1);
+} else {
+point[index++] = a;
+}
+}
+ 
+public void divideTriangle(Vector2f a, Vector2f b, Vector2f c, int count){
+divideSide(a, b, count);
+divideSide(b, c, count);
+divideSide(c, a, count);
+}
+ 
+public void initSnowFlake(){
+Vector2f[] vertices = { 
+new Vector2f(-0.6f, -0.6f),
+new Vector2f(0f, 0.6f),
+new Vector2f(0.6f, -0.6f),
+};
+index = 0;
+divideTriangle(vertices[0], vertices[1], vertices[2], numTimesToSubdivide);
+}
+/**
+* @param args
+*/
+public static void main(String[] args) {
+Snowflake snf = new Snowflake();
+snf.start();
+}
 
-	private float fovy = 60.0f;
-	private int numTimesToSubdivide = 5;
-	private int numFlakes = (int)Math.pow(4, numTimesToSubdivide);
-	private int numVertices = 3 * numFlakes;
-	
-	private class KochSet {
-		Vector2f l = null;
-		Vector2f m = null;
-		Vector2f n = null;
-		Vector2f o = null;
-		Vector2f p = null;
-	}
-	
-	double deg60 = 60.0f * Math.PI / 180.0;
-	
-	private int index = 0;
-	
-	Vector2f[] point = new Vector2f[numVertices];
-	
-	public KochSet computeKochSet(Vector2f s, Vector2f d){
-		
-		KochSet k = new KochSet();
-		k.l = s;
-		k.m = new Vector2f(s.x + (d.x - s.x)/3.0f, s.y + (d.y - s.y)/3.0f);
-		k.o = new Vector2f(s.x + (d.x - s.x)*2/3.0f, s.y + (d.y - s.y)*2/3.0f);
-		float px = (float)(Math.cos(deg60) * (k.o.x - k.m.x)- Math.sin(deg60) * (k.o.y - k.m.y)+ k.m.x);  
-		float py = (float)(Math.sin(deg60) * (k.o.x - k.m.x)+ Math.cos(deg60) * (k.o.y - k.m.y)+ k.m.x);  
-		k.n = new Vector2f(px, py);
-		k.p = d;
-		return k;
-		
-	}
-	
-	public void divideSide(Vector2f a, Vector2f b, int count){
-		
-		if (count > 0){
-			KochSet kab = computeKochSet(a, b);
-			
-			divideSide(kab.l, kab.m, count - 1);
-			divideSide(kab.m, kab.n, count - 1);
-			divideSide(kab.n, kab.o, count - 1);
-			divideSide(kab.o, kab.p, count - 1);
+@Override
+protected void initGL() {
+try {
+GLContext.useContext(canvas);
+ 
+} catch (LWJGLException e){
+e.printStackTrace();
+}
+GL11.glMatrixMode(GL11.GL_PROJECTION);
+GL11.glLoadIdentity();
+ 
+float aspect = WIDTH/ (float) HEIGHT;
+GL11.glViewport(0, 0, WIDTH, HEIGHT);
+GLU.gluPerspective(fovy, aspect, zNear, zFar);
+GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
+ 
+GL11.glClearColor(0, 0, 0, 0);
+GL11.glClearDepth(1.0);
+GL11.glEnable(GL11.GL_DEPTH_TEST);
+ 
+initSnowFlake();
+}
 
-		} else {
-			point[index++] = a;
-		}
-	}
-	
-	public void divideTriangle(Vector2f a, Vector2f b, Vector2f c, int count){
-		divideSide(a, b, count);
-		divideSide(b, c, count);
-		divideSide(c, a, count);
-	}
-	
-	public void initSnowFlake(){
-		Vector2f[] vertices = { 
-				new Vector2f(-0.6f, -0.6f),
-				new Vector2f(0f, 0.6f),
-				new Vector2f(0.6f, -0.6f),
-		};
-		index = 0;
-		divideTriangle(vertices[0], vertices[1], vertices[2], numTimesToSubdivide);
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Snowflake snf = new Snowflake();
-		snf.start();
-	}
+@Override
+protected void update(int delta) {
+updateFPS();
+}
 
-	@Override
-	protected void initGL() {
-		
-		try{
-			GLContext.useContext(canvas);
-		} catch (LWJGLException e){
-			e.printStackTrace();
-		}
-		
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		
-		float aspect = WIDTH/(float)HEIGHT;
-		GLU.gluPerspective(fovy, aspect, zNear, zFar);
-		GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
-		
-		GL11.glViewport(0, 0, WIDTH, HEIGHT);
-		GL11.glClearColor(0, 0, 0, 0);
-		
-		GL11.glColor3f(1.0f, 0 ,0);
-		GL11.glLineWidth(3.0f);
-		
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		
-	}
+@Override
+protected void resetGL() {
+ 
+GL11.glMatrixMode(GL11.GL_PROJECTION);
+GL11.glLoadIdentity();
+ 
+Rectangle bounds = canvas.getBounds();
+float aspect = WIDTH/ (float) HEIGHT;
+ 
+GL11.glViewport(0, 0, bounds.width, bounds.height);
+GLU.gluPerspective(fovy, aspect, zNear, zFar);
+GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
+ 
+GL11.glMatrixMode(GL11.GL_MODELVIEW);
+GL11.glLoadIdentity();
+}
 
-	@Override
-	protected void update(int delta) {
-		updateFPS();
-	}
+@Override
+protected void renderGL() {
+GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+GL11.glMatrixMode(GL11.GL_PROJECTION);
+GL11.glLoadIdentity();
+ 
+Rectangle bounds = canvas.getBounds();
+ 
+float aspect = WIDTH/ (float) HEIGHT;
+GL11.glViewport(0, 0, bounds.width, bounds.height);
+GLU.gluPerspective(fovy, aspect, zNear, zFar);
+GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
+ 
+GL11.glMatrixMode(GL11.GL_MODELVIEW);
+GL11.glPushMatrix();
+ 
+GL11.glColor3f(0, 1, 1);
+GL11.glLineWidth(3.0f);
+for (int i = 0; i < 180; i++){
+renderSnowFlake();
+GL11.glTranslatef(0, 0, -1);
+}
+GL11.glPopMatrix();
+ 
+// setup projection
+}
 
-	@Override
-	protected void resetGL() {
-	
-		Rectangle bounds = canvas.getBounds();
-		GL11.glViewport(0, 0, bounds.width, bounds.height);
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		
-		float aspect = bounds.width/bounds.height;
-		GLU.gluPerspective(fovy, aspect, zNear, zFar);
-		GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
-		
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		GL11.glLoadIdentity();
-	
-	}
+private void renderSnowFlake() {
+GL11.glBegin(GL11.GL_LINE_LOOP);
+for (int i = 0; i < numVertices; i++){
+GL11.glVertex3f(point[i].x, point[i].y, 0.25f);
+}
+GL11.glEnd();
+}
 
-	@Override
-	protected void renderGL() {
-		
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		
-	
-		
-		Rectangle bounds = canvas.getBounds();
-		GL11.glViewport(0, 0, bounds.width, bounds.height);
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		
-		float aspect = bounds.width/bounds.height;
-		GLU.gluPerspective(fovy, aspect, zNear, zFar);
-		GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
-		
-		initSnowFlake();
-		
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		GL11.glPushMatrix();
-		renderSnowflake();
-		GL11.glPopMatrix();
-		
-	}
-
-	private void renderSnowflake() {
-		
-		GL11.glBegin(GL11.GL_LINE_LOOP);
-		for(int i = 0; i < numVertices; i++){
-			GL11.glVertex3f(point[i].x, point[i].y, 0.25f);
-		}
-		
-	}
-
-	@Override
-	protected void initTextures() {
-		// TODO Auto-generated method stub
-		
-	}
-
+@Override
+protected void initTextures() {
+// TODO Auto-generated method stub
+ 
+}
 }
