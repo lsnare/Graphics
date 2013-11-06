11327563b0984eb594bf07cfd1aae30034a24605
Lucian Snare <lsnare1@gmail.com>
2013-11-06 13:28:47 -0500

    finished

--- Sierpinsky/src/sierpinsky/Sierpinsky.java
+++ Sierpinsky/src/sierpinsky/Sierpinsky.java
@@ -7,18 +7,18 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GLContext;
 import org.lwjgl.util.glu.GLU;
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
 
 public class Sierpinsky extends SkelGL{
-
-	class Point{
-		float x,y;
-		Point(float x, float y){
-			this.x=x;
-			this.y=y;
-		}
-	}
 	
-	Point[] path = new Point[7];
+	ArrayList<Vector2f> points = new ArrayList<Vector2f>();
+	
+	Vector3f[] colors = {
+		new Vector3f(1,0,0),
+		new Vector3f(0,1,0),
+		new Vector3f(0,0,1)
+	};
 	
 	@Override
 	protected void initGL() {
@@ -40,6 +40,8 @@ public class Sierpinsky extends SkelGL{
 		GL11.glClearDepth(1.0);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		
+		//equilateral triangle of size 3 filled with 1,000,000 dots
+		initSierpinsky(10,10,100000);
 	}
 
 	@Override
@@ -66,7 +68,7 @@ public class Sierpinsky extends SkelGL{
 
 	@Override
 	protected void renderGL() {
-		// TODO Auto-generated method stub
+		
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
@@ -81,13 +83,9 @@ public class Sierpinsky extends SkelGL{
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		GL11.glPushMatrix();
 
-		GL11.glColor3f(0, 1, 1);
 		GL11.glLineWidth(3.0f);
 
-		//points
-		
-			GL11.glColor3f(1, 0, 0);
-			renderSerpinsky();
+		renderSerpinsky();
 		
 		GL11.glPopMatrix();
 	}
@@ -98,39 +96,56 @@ public class Sierpinsky extends SkelGL{
 		
 	}
 	
-	public void initSierpinsky(){
-		Point a = new Point(1.5f,3f);
-		Point b = new Point(0,0);
-		Point c = new Point(3f,0);
-		Point start = new Point(0.5f,0.5f);
+	public void initSierpinsky(float xMax, float yMax, int pointNum){
+		Vector2f a = new Vector2f(0.0f,0.0f);
+		Vector2f b = new Vector2f(xMax/2f, yMax);
+		Vector2f c = new Vector2f(xMax, 0f);
+		//TODO: Fix the random number
+		Vector2f start = new Vector2f(xMax/2, yMax/2);
+		Vector2f vertex = randomVertex(a,b,c);
+		Vector2f midpoint = new Vector2f((vertex.x + start.x)/2.0f, (vertex.y + start.y)/2.0f);
 		
-		path.add(a);
-		path.add(b);
-		path.add(c);
+		points.add(a);
+		points.add(b);
+		points.add(c);
+		points.add(midpoint);
 		
+		for(int i = 1; i < pointNum; i++){
+			vertex = randomVertex(a,b,c);
+			points.add(new Vector2f((vertex.x + points.get(i-1).x)/2.0f, (vertex.y + points.get(i-1).y)/2.0f));
+		}
+				
+	}
+	
+	
+	//randomly picks a vertex on the triangle
+	public Vector2f randomVertex(Vector2f a, Vector2f b, Vector2f c){
+		
+		Vector2f selected;
 		double ran = Math.random();
 		
-		if(ran < .3)
-			path.add(new Point((a.x-start.x)/2, (a.y-start.y)/2));
-		else if(ran > .3)
-			path.add(new Point((b.x-start.x)/2, (b.y-start.y)/2));
-		else
-			path.add(new Point((c.x-start.x)/2, (c.y-start.y)/2));
-		
-		for(int i = 0; i < 10; i++){
-			path.add(new Point(path,0));
+		if(ran < 0.3){
+			selected = a;
+		}else if(ran >= 0.3 && ran < 0.6){
+			selected = b;
+		}else{
+			selected = c; 
 		}
+		
+		return selected;
+		
 	}
+	
 	
 	public void renderSerpinsky(){
 		GL11.glBegin(GL11.GL_POINTS);
-		float ran = (float)Math.random()*6;
-		GL11.glVertex2f(0,0);
-		GL11.glVertex2f(3,6);
-		GL11.glVertex2f(6,0);
-		for (int i = 0; i < 100; i++) {
-			GL11.glVertex2f(i-ran,i);
+		
+		for (int i = 0; i < points.size(); i++) {
+			//multi-colored gasket
+			GL11.glColor3f(colors[i%3].x, colors[i%3].y, colors[i%3].z);
+			GL11.glVertex2f(points.get(i).x, points.get(i).y);
 		}
+		
 		GL11.glEnd();
 	}
 	
