aa35b30ed2d49d742834932e412be1c7478d108c
Lucian Snare <lsnare@lhup.edu>
2013-11-06 12:07:44 -0500

    whoaaa

--- 3DSierpinsky/src/sierpinsky/Sierpinsky.java
+++ 3DSierpinsky/src/sierpinsky/Sierpinsky.java
@@ -2,6 +2,7 @@ package sierpinsky;
 
 import java.nio.FloatBuffer;
 
+import org.eclipse.swt.graphics.Rectangle;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
@@ -12,7 +13,7 @@ import textureMapper.TextureMapper;
 
 public class Sierpinsky extends SkelGL{
 
-	private int numTimesToSubdivide = 2;
+	private int numTimesToSubdivide = 8;
 	private int numTetra = (int) Math.pow(4.0, numTimesToSubdivide);
 	 
 	private int numTriangles = 4*numTetra;
@@ -31,6 +32,13 @@ public class Sierpinsky extends SkelGL{
 			new Vector3f(1, -.5f, .5f)
 	};
 	
+	Vector3f[] palette = {
+			new Vector3f(1,0,0),
+			new Vector3f(0,1,0),
+			new Vector3f(0,0,1),
+			new Vector3f(1,1,0)
+		};
+	
 	TextureMapper tmap = null;
 	
 	Vector2f[] textureBounds = {
@@ -41,7 +49,7 @@ public class Sierpinsky extends SkelGL{
 	
 	Vector2f[] textures = new Vector2f[numVertices]; 
 	
-	float rotation = 20.0f;
+	float rotation = 0.5f;
 	
 	private FloatBuffer matSpecular;
 	private FloatBuffer lightPosition;
@@ -113,9 +121,15 @@ public class Sierpinsky extends SkelGL{
 		
 	}
 
-	private void triangle(Vector3f a, Vector3f c, Vector3f d, int i) {
-		// TODO Auto-generated method stub
+	private void triangle(Vector3f a, Vector3f b, Vector3f c, int color) {
 		
+		Vector3f u = Vector3f.sub(a, b, null);
+		Vector3f v = Vector3f.sub(c, b, null);
+		Vector3f n = Vector3f.cross(u, v, null);
+		
+		points[index] = a; normals[index] = n; colors[index] = palette[color]; index++;
+		points[index] = b; normals[index] = n; colors[index] = palette[color]; index++;
+		points[index] = c; normals[index] = n; colors[index] = palette[color]; index++;
 	}
 
 	private void initLightArrays() {
@@ -128,10 +142,10 @@ public class Sierpinsky extends SkelGL{
         lightPosition.put(-1f).put(1f).put(0.5f).put(0f).flip();
 
         whiteLight = BufferUtils.createFloatBuffer(4);
-        whiteLight.put(0.5f).put(0.5f).put(0.5f).put(1.0f).flip();
+        whiteLight.put(1f).put(1f).put(1f).put(1.0f).flip();
         
         lModeAmbient = BufferUtils.createFloatBuffer(4);
-        lModeAmbient.put(0.1f).put(0.1f).put(0.1f).put(1f).flip();
+        lModeAmbient.put(0.5f).put(0.1f).put(0.1f).put(1f).flip();
        
         GL11.glShadeModel(GL11.GL_SMOOTH);
         GL11.glMaterial(GL11.GL_FRONT, GL11.GL_SPECULAR, matSpecular);
@@ -151,17 +165,61 @@ public class Sierpinsky extends SkelGL{
 
 	@Override
 	protected void update(int delta) {
-	
+		if(animate){
+			rotation += 0.01f * delta;
+		}
 	}
 
 	@Override
 	protected void resetGL() {
 		
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		
+		Rectangle bounds = canvas.getBounds();
+		
+		float aspect = bounds.width/bounds.height;
+		GLU.gluPerspective(fovy, aspect, zNear, zFar);
+		GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
+		GL11.glViewport(0, 0, bounds.width, bounds.height);
+		
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
 		
 	}
 
 	@Override
 	protected void renderGL() {
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		
+		Rectangle bounds = canvas.getBounds();
+		float aspect = bounds.width/bounds.height;
+		
+		GLU.gluPerspective(fovy, aspect, zNear, zFar);
+		GLU.gluLookAt(eyex, eyey, eyez, 0, 0, 0, 0, 1, 0);
+		GL11.glViewport(0, 0, bounds.width, bounds.height);
+		
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		
+		GL11.glPushMatrix();
+		GL11.glRotatef(rotation, 1,0,0);
+		GL11.glRotatef(rotation, 0,1,0);
+		GL11.glRotatef(rotation, 0,0,1);
+		renderGasket();
+		GL11.glPopMatrix();
+	}
+
+	private void renderGasket() {
+		GL11.glBegin(GL11.GL_TRIANGLES);
+		for(int i = 0; i < numVertices; i++){
+			GL11.glNormal3f(normals[i].x, normals[i].y, normals[i].z);
+			GL11.glColor3f(colors[i].x, colors[i].y, colors[i].z);
+			GL11.glVertex3f(points[i].x, points[i].y, points[i].z);
+		}
+		GL11.glEnd();
 		
 	}
 
@@ -172,7 +230,8 @@ public class Sierpinsky extends SkelGL{
 	}
 	
 	public static void main(String[] args){
-		
+		Sierpinsky s = new Sierpinsky();
+		s.start();
 	}
 
 }
